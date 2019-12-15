d36ec69ebaf2d5b0eaefb4937a6b578af79d6791
PaulElcampeon <pauloladele@live.co.uk>
2019-12-15 20:00:56 +0000

    Add custom constructor.

--- src/main/java/com/pauloladele/ironsafe/dto/CreateUserRequest.java
+++ src/main/java/com/pauloladele/ironsafe/dto/CreateUserRequest.java
@@ -9,4 +9,13 @@ public class CreateUserRequest {
     private String confirmEmail;
     private String password;
     private String confirmPassword;
+
+    public CreateUserRequest() {}
+
+    public CreateUserRequest(String email, String confirmEmail, String password, String confirmPassword) {
+        this.email = email;
+        this.confirmEmail = confirmEmail;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
 }
