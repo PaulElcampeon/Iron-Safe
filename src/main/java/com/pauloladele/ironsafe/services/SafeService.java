d5c26ee77996ac85fa19cbc32abcacfe3bc99426
PaulElcampeon <pauloladele@live.co.uk>
2019-12-15 20:04:58 +0000

    Added functionality to remove and add credentials and added arguments to methods.

--- src/main/java/com/pauloladele/ironsafe/services/SafeService.java
+++ src/main/java/com/pauloladele/ironsafe/services/SafeService.java
@@ -1,16 +1,25 @@
 package com.pauloladele.ironsafe.services;
 
+import com.mongodb.client.result.UpdateResult;
 import com.pauloladele.ironsafe.dto.AddCredentialsRequest;
 import com.pauloladele.ironsafe.dto.RemoveCredentialsRequest;
+import com.pauloladele.ironsafe.models.Credential;
 import com.pauloladele.ironsafe.models.Safe;
 import com.pauloladele.ironsafe.repositories.SafeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
 
 @Service
 public class SafeService implements SafeInterface {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @Autowired
     private SafeRepository repository;
@@ -26,12 +35,29 @@ public class SafeService implements SafeInterface {
     }
 
     @Override
-    public boolean addCredentials(AddCredentialsRequest addCredentialsMessage) {
-        return false;
+    public boolean addCredentials(AddCredentialsRequest addCredentialsMessage, String email) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("_id").is(email));
+
+        Update update = new Update();
+        update.addToSet("credentials", new Credential(addCredentialsMessage.getKey(), addCredentialsMessage.getValue()));
+
+        UpdateResult updateResult = mongoTemplate.updateFirst(query, update, Safe.class);
+
+        return updateResult.getModifiedCount() == 1;
     }
 
     @Override
-    public boolean removeCredentials(RemoveCredentialsRequest removeCredentialsMessage) {
-        return false;
+    public boolean removeCredentials(RemoveCredentialsRequest removeCredentialsMessage, String email) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("_id").is(email)
+                .and("credentials.key").is(removeCredentialsMessage.getKey()));
+
+        Update update = new Update();
+        update.pull("credentials", new Credential(removeCredentialsMessage.getKey(), removeCredentialsMessage.getValue()));
+
+        UpdateResult updateResult = mongoTemplate.updateFirst(query, update, Safe.class);
+
+        return updateResult.getModifiedCount() == 1;
     }
 }
