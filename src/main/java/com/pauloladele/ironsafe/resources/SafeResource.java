f0fd870c4180e9a889a0aec7533968a8db4ebd89
PaulElcampeon <pauloladele@live.co.uk>
2019-12-15 20:02:22 +0000

    Added functionality for adding and removing credentials.

--- src/main/java/com/pauloladele/ironsafe/resources/SafeResource.java
+++ src/main/java/com/pauloladele/ironsafe/resources/SafeResource.java
@@ -1,10 +1,13 @@
 package com.pauloladele.ironsafe.resources;
 
+import com.pauloladele.ironsafe.dto.AddCredentialsRequest;
+import com.pauloladele.ironsafe.dto.RemoveCredentialsRequest;
 import com.pauloladele.ironsafe.models.Safe;
 import com.pauloladele.ironsafe.services.SafeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +27,21 @@ public class SafeResource {
         final Safe safe = safeService.getSafe(principal.getName());
 
         return new ResponseEntity<>(safe, HttpStatus.ACCEPTED);
+    }
+
+    @RequestMapping(value = "/add/credential", method = RequestMethod.POST)
+    public ResponseEntity<?> addCredential(@RequestBody AddCredentialsRequest addCredentialsRequest, Principal principal) {
+
+        final boolean success = safeService.addCredentials(addCredentialsRequest, principal.getName());
+
+        return new ResponseEntity<>(success, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/remove/credential", method = RequestMethod.DELETE)
+    public ResponseEntity<?> removeCredential(@RequestBody RemoveCredentialsRequest removeCredentialsRequest, Principal principal) {
+
+        final boolean success  = safeService.removeCredentials(removeCredentialsRequest, principal.getName());
+
+        return new ResponseEntity<>(success, HttpStatus.ACCEPTED);
     }
 }
