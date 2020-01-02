75ea27c0cd17dc3b2ca9aba2aa3e3598751447a5
PaulElcampeon <pauloladele@live.co.uk>
2020-01-02 18:59:56 +0000

    Removed uneeded code.

--- src/main/java/com/pauloladele/ironsafe/resources/SafeResource.java
+++ src/main/java/com/pauloladele/ironsafe/resources/SafeResource.java
@@ -7,10 +7,7 @@ import com.pauloladele.ironsafe.services.SafeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -34,7 +31,7 @@ public class SafeResource {
 
         final boolean success = safeService.addCredentials(addCredentialsRequest, principal.getName());
 
-        return new ResponseEntity<>(success, HttpStatus.CREATED);
+        return new ResponseEntity<>(success, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/remove/credential", method = RequestMethod.DELETE)
