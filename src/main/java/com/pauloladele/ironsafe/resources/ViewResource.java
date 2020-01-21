4e6ca4f1db3f43041dee487618eb69aebdb20ad4
PaulElcampeon <pauloladele@live.co.uk>
2020-01-21 13:05:32 +0000

    Resource to serve static files.

---
+++ src/main/java/com/pauloladele/ironsafe/resources/ViewResource.java
@@ -0,0 +1,14 @@
+package com.pauloladele.ironsafe.resources;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ViewResource {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String homePage() {
+        return "home";
+    }
+}
