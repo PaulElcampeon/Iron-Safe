e6694e71d5222c59333a1771edbbb88b90e27b53
PaulElcampeon <pauloladele@live.co.uk>
2020-01-22 18:15:14 +0000

    react app bundled.

---
+++ src/main/resources/static/precache-manifest.8196a4df8e3d34762f36dd033ba467ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b69dcb17c164ba7ddbb4540af9721be5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09337fd7684c911c8fb1",
+    "url": "/static/css/main.d9cc9ffa.chunk.css"
+  },
+  {
+    "revision": "727426ef67da8b14fda7",
+    "url": "/static/js/2.1e636ba9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.1e636ba9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09337fd7684c911c8fb1",
+    "url": "/static/js/main.b5d9b0e5.chunk.js"
+  },
+  {
+    "revision": "4a87e149f40a08219da0",
+    "url": "/static/js/runtime-main.c8dc4db3.js"
+  }
+]);
